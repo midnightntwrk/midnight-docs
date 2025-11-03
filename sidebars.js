1cf0ac2c5e57ecc7cb3e953932812e10a1fcdece
nebelsprachen <joanxedwards@gmail.com>
2025-11-03 17:56:28 +0000

    Remove sidebar item

--- sidebars.js
+++ sidebars.js
@@ -126,11 +126,6 @@ module.exports = {
           label: "Fix Package Repository Issue"
         },
         {
-          type: "doc",
-          id: "develop/guides/compact-javascript-runtime",
-          label: "The Compact JavaScript runtime"
-        },
-        {
           type: "category",
           label: "Query the blockchain",
           items: ["develop/nodes-and-dapps/nodes-endpoints"]
