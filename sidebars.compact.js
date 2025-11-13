9aa37ed3e49a83b937c01330f5910be0e12833af
0xisk <iskander.andrews@openzeppelin.com>
2025-11-13 15:06:34 +0100

    refactor: restructure the compact lang ref

--- sidebars.compact.js
+++ sidebars.compact.js
@@ -8,7 +8,110 @@ const sidebars = {
       link: { type: "doc", id: "index" },
       items: [
         "writing",               // docs/develop/reference/compact/writing.md(x)
-        "lang-ref",              // docs/develop/reference/compact/lang-ref.md(x)
+        {
+          type: "category",
+          label: "Compact reference",
+          link: { type: "doc", id: "lang-ref/index" },
+          items: [
+            {
+              type: "category",
+              label: "Compact Types",
+              link: { type: "doc", id: "lang-ref/types" },
+              items: [
+                "lang-ref/types/primitive-types",
+                {
+                  type: "category",
+                  label: "User-defined types",
+                  link: { type: "doc", id: "lang-ref/types/user-defined-types" },
+                  items: [
+                    "lang-ref/types/user-defined-types/structure-types",
+                    "lang-ref/types/user-defined-types/enumeration-types",
+                    "lang-ref/types/user-defined-types/contract-types",
+                  ],
+                },
+                "lang-ref/types/generic-parameter-references",
+                "lang-ref/types/subtyping-and-least-upper-bounds",
+                "lang-ref/types/default-values",
+                "lang-ref/types/representations-in-typescript",
+              ],
+            },
+            "lang-ref/include-files",
+            "lang-ref/modules-exports-and-imports",
+            "lang-ref/top-level-exports",
+            {
+              type: "category",
+              label: "Circuits",
+              link: { type: "doc", id: "lang-ref/circuits" },
+              items: [
+                "lang-ref/circuits/pure-and-impure-circuits",
+                {
+                  type: "category",
+                  label: "Statements",
+                  link: { type: "doc", id: "lang-ref/circuits/statements" },
+                  items: [
+                    "lang-ref/circuits/statements/for-loop",
+                    "lang-ref/circuits/statements/if-statement",
+                    "lang-ref/circuits/statements/return-statement",
+                    "lang-ref/circuits/statements/assert-statement",
+                    "lang-ref/circuits/statements/const-binding-statement",
+                  ],
+                },
+                {
+                  type: "category",
+                  label: "Expressions",
+                  link: { type: "doc", id: "lang-ref/circuits/expressions" },
+                  items: [
+                    "lang-ref/circuits/expressions/literals",
+                    "lang-ref/circuits/expressions/variable-references",
+                    "lang-ref/circuits/expressions/default-values-of-a-type",
+                    "lang-ref/circuits/expressions/circuit-and-witness-calls",
+                    "lang-ref/circuits/expressions/structure-creation",
+                    "lang-ref/circuits/expressions/tuple-creation",
+                    "lang-ref/circuits/expressions/parenthesized-expressions",
+                    "lang-ref/circuits/expressions/sequence-expressions",
+                    "lang-ref/circuits/expressions/ledger-expressions",
+                    "lang-ref/circuits/expressions/element-and-member-access-expressions",
+                    "lang-ref/circuits/expressions/boolean-negation-expressions",
+                    "lang-ref/circuits/expressions/binary-arithmetic-expressions",
+                    "lang-ref/circuits/expressions/type-cast-expressions",
+                    "lang-ref/circuits/expressions/relational-comparison-expressions",
+                    "lang-ref/circuits/expressions/short-circuit-logical-expressions",
+                    "lang-ref/circuits/expressions/conditional-expressions",
+                    "lang-ref/circuits/expressions/map-and-fold-expressions",
+                    "lang-ref/circuits/expressions/ledger-assignment-expressions",
+                  ],
+                },
+              ],
+            },
+            "lang-ref/declaring-witnesses-for-private-state",
+            {
+              type: "category",
+              label: "Declaring and maintaining public state",
+              link: { type: "doc", id: "lang-ref/declaring-and-maintaining-public-state" },
+              items: [
+                "lang-ref/declaring-and-maintaining-public-state/ledger-state-types",
+                "lang-ref/declaring-and-maintaining-public-state/nested-state-types-in-the-map-type",
+                "lang-ref/declaring-and-maintaining-public-state/sealed-and-unsealed-ledger-fields",
+              ],
+            },
+            {
+              type: "category",
+              label: "Contract constructor",
+              link: { type: "doc", id: "lang-ref/contract-constructor" },
+              items: [
+                "lang-ref/contract-constructor/runtime-representations-and-type-bounds-checks",
+              ],
+            },
+            {
+              type: "category",
+              label: "TypeScript target",
+              link: { type: "doc", id: "lang-ref/typescript-target" },
+              items: [
+                "lang-ref/typescript-target/structure-of-the-exported-typescript",
+              ],
+            },
+          ],
+        },
         "compact-grammar",       // docs/develop/reference/compact/compact-grammar.md(x)
         "ledger-adt",            // docs/develop/reference/compact/ledger-adt.md(x)
         "opaque_data",           // docs/develop/reference/compact/opaque_data.md(x)
