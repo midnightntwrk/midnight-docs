4c48c3053181d1dff0e0f1e45efd7ab51f90d75f
nebelsprachen <joanxedwards@gmail.com>
2025-11-16 20:53:52 -0500

    Fix component for render
    Signed-off-by: nebelsprachen <joanxedwards@gmail.com>

--- src/components/video-player/VideoPlayer.tsx
+++ src/components/video-player/VideoPlayer.tsx
@@ -1,14 +1,52 @@
-import React from "react";
-import { FC } from "react";
-import ReactPlayer from "react-player";
+import React, { FC } from "react";
 
 type Props = {
   url: string;
-  autoplay?: boolean; 
+  autoplay?: boolean;
 };
 
+let ReactPlayer: any;
+if (typeof window !== "undefined") {
+  ReactPlayer = require("react-player").default;
+}
+
 const VideoPlayer: FC<Props> = ({ url, autoplay = false }) => {
-  return <ReactPlayer controls playing={autoplay} width="100%" height="450px" url={url} />;
+  if (typeof window === "undefined" || !ReactPlayer) {
+    return (
+      <div
+        style={{
+          position: "relative",
+          paddingTop: "56.25%",
+          borderRadius: "12px",
+          background: "#000",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            inset: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "0.8rem",
+            opacity: 0.6,
+          }}
+        >
+          Video will load in the browser
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <ReactPlayer
+      controls
+      playing={autoplay}
+      width="100%"
+      height="450px"
+      url={url}
+    />
+  );
 };
 
 export default VideoPlayer;
