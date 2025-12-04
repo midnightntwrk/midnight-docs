c13aaae8a3c31d532cea0b5662e3e1db7ac5c0ef
nebelsprachen <joanxedwards@gmail.com>
2025-12-04 16:12:52 +0000

    Links correct (#431)
    * fix: make styling more accessible
    
    * fix: make styling more accessible
    
    * fix: make styling more accessible
    
    * fix: make styling more accessible
    
    * fix: make styling more accessible
    
    * fix: make styling more accessible

---
+++ src/theme/DocSidebar/Desktop/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CollapseButton from '@theme/DocSidebar/Desktop/CollapseButton';
+import DocSidebarItems from '@theme/DocSidebar/Desktop/Items';
+import EnvSelector from '@site/src/components/EnvSelector';
+import styles from './styles.module.css';
+
+export default function DocSidebarDesktop({ path, sidebar }) {
+  return (
+    <div className={styles.sidebarContainer}>
+      <div className={styles.collapseRow}>
+        <CollapseButton />
+      </div>
+      <div className={styles.selectorRow}>
+        <EnvSelector />
+      </div>
+      <DocSidebarItems items={sidebar} activePath={path} />
+    </div>
+  );
+}
