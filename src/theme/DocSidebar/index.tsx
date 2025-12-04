ee4c9210030fdfc7208698903bd0873182187b83
nebelsprachen <joanxedwards@gmail.com>
2025-12-04 13:54:26 +0000

    fix: make styling more accessible

--- src/theme/DocSidebar/index.tsx
+++ src/theme/DocSidebar/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import DocSidebar from '@theme-original/DocSidebar';
+import OriginalDocSidebar from '@theme-original/DocSidebar';
 import EnvSelector from '@site/src/components/EnvSelector';
 
-export default function DocSidebarWrapper(props: any) {
+export default function DocSidebarWrapper(props) {
   if (!props.sidebar) return null;
 
   return (
-    <div className={props.className}>
+    <>
       <EnvSelector />
-      <DocSidebar {...props} />
-    </div>
+      <OriginalDocSidebar {...props} />
+    </>
   );
 }
