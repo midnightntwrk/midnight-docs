68e95c80488a90cbaa0911b42544aa52d1b7b54e
pataei <paris.ataei@gmail.com>
2026-03-05 15:03:53 -0500

    another change to add keywords to sidebar menue

--- sidebars.compact.js
+++ sidebars.compact.js
@@ -10,6 +10,7 @@ const sidebars = {
         "writing",               // docs/develop/reference/compact/writing.md(x)
         "lang-ref",              // docs/develop/reference/compact/lang-ref.md(x)
         "compact-grammar",       // docs/develop/reference/compact/compact-grammar.md(x)
+        "all-keywords",          // docs/develop/reference/compact/all-keywords.md(x)
         "ledger-adt",            // docs/develop/reference/compact/ledger-adt.md(x)
         "opaque_data",           // docs/develop/reference/compact/opaque_data.md(x)
         "explicit-disclosure",   // docs/develop/reference/compact/explicit-disclosure.md(x)
