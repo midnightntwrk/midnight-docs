1299e05d592f3f931156dafe51665f08eb247224
Ashley Pollard <ashley.pollard@infinum.com>
2026-09-02 18:26:45 -0400

    configure tech article schema for docs and blog

--- src/theme/Navbar/Layout/index.tsx
+++ src/theme/Navbar/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useEffect, useState } from "react";
 import clsx from "clsx";
 import { useThemeConfig } from "@docusaurus/theme-common";
 import {
@@ -7,6 +7,8 @@ import {
 } from "@docusaurus/theme-common/internal";
 import { translate } from "@docusaurus/Translate";
 import NavbarMobileSidebar from "@theme/Navbar/MobileSidebar";
+import { useBlogPost } from "@docusaurus/plugin-content-blog/lib/client/contexts.js";
+import { useLocation } from "@docusaurus/router";
 import styles from "./styles.module.css";
 
 function NavbarBackdrop(props) {
@@ -24,6 +26,45 @@ export default function NavbarLayout({ children }: PropsWithChildren) {
   } = useThemeConfig();
   const mobileSidebar = useNavbarMobileSidebar();
   const { navbarRef, isNavbarVisible } = useHideableNavbar(hideOnScroll);
+
+  // Schema
+  const [techArticleSchemaType, setSchemaData] = useState<Object | null>(null);
+  const location = useLocation().pathname;
+
+  if (
+    location.includes("/blog") &&
+    location !== "/blog" &&
+    !location.includes("/index")
+  ) {
+    const { metadata, frontMatter } = useBlogPost();
+
+    const getProdUrl = (path = "/"): string => {
+      const baseUrl = "https://docs.midnight.network";
+      return new URL(path, baseUrl).href;
+    };
+
+    useEffect(() => {
+      setSchemaData({
+        "@context": "https://schema.org",
+        "@type": "TechArticle",
+        headline: metadata?.title || frontMatter?.title,
+        author: {
+          "@type": "Person",
+          name: metadata?.authors[0].name || frontMatter?.authors || "Midnight"
+        },
+        datePublished: metadata?.date
+          ? new Date(metadata?.date).toISOString()
+          : frontMatter?.date
+            ? new Date(frontMatter?.date).toISOString()
+            : "",
+        // proficiencyLevel: undefined,
+        // dependencies: undefined,
+        description: metadata?.description || frontMatter?.description,
+        image: getProdUrl(frontMatter?.image)
+      });
+    }, [location]);
+  }
+
   return (
     <nav
       ref={navbarRef}
@@ -50,6 +91,17 @@ export default function NavbarLayout({ children }: PropsWithChildren) {
       {children}
       <NavbarBackdrop onClick={mobileSidebar.toggle} />
       <NavbarMobileSidebar />
+      {techArticleSchemaType && (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(techArticleSchemaType).replace(
+              /</g,
+              "\\u003c"
+            )
+          }}
+        />
+      )}
     </nav>
   );
 }
